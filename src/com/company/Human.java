11841cb98b987ed750702c75cb4ea9732fbdcc6c
Bljek <BlackDrugsGaming@gmail.com>
2022-01-16 16:19:44 +0100

    Zadanie 3 i 4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,8 +1,55 @@
 package com.company;
+import java.time.LocalDateTime;
+
 public class Human {
+    public static Object getSalary;
+    public static Double pensja;
     String firstName;
     String lastName;
     Phone phone;
     Animal pet;
     Car car;
+
+    private Double salary;
+    private Double lastSalary;
+    private Integer countOfGetting = 0;
+    private LocalDateTime lastGet;
+
+    public void setSalary(Double salary) {
+        if (salary < 0) {
+            System.out.println("Uwaga! Pensja nie może być niższa niż 0!");
+
+        } else if (salary == null) {
+            double a = 2600.00;
+            this.salary = a;
+            System.out.println("\nNowe dane zostały wysłane do systemu księgowego. \nNie zapomnij odebrać aneksu do umowy od Pani Hanny z działu kadr. \nZUS i US mają już informację o zmianie Twoich dochodów. Próby ukrycia ich nie mają sensu.");
+
+        } else {
+            double a = salary;
+            a = salary;
+            a = a * 100;
+            a = Math.floor(a);
+            a = a / 100;
+            this.salary = a;
+            System.out.println("\nNowe dane zostały wysłane do systemu księgowego. \nNie zapomnij odebrać aneksu do umowy od Pani Hanny z działu kadr. \nZUS i US mają już informację o zmianie Twoich dochodów. Próby ukrycia ich nie mają sensu.");
+        }
+    }
+    public Double getSalary() {
+        if (countOfGetting == 0) {
+            countOfGetting++;
+            lastSalary = this.salary;
+            lastGet = LocalDateTime.now();
+            System.out.println("\nLiczba wglądów w Twoją pensję: " + countOfGetting + "\nCzas ostatniego wglądu: " + lastGet + "\nTwoja pensja wtedy: " + lastSalary + "\n\nTwoja pensja wynosi:");
+            return this.salary;
+        }
+        else {
+            countOfGetting++;
+            System.out.println("\nLiczba wglądów w Twoją pensję: " + countOfGetting + "\nCzas ostatniego wglądu: " + lastGet + "\nTwoja pensja wtedy: " + lastSalary + "\n\nTwoja pensja wynosi:");
+            lastSalary = this.salary;
+            lastGet = LocalDateTime.now();
+            return this.salary;
+        }
+
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,48 @@ public class Main {
     public static void main(String[] args) {
         Animal dog = new Animal("Dog");
         dog.name = "Hacia";
-        System.out.println("Hi, I'm " + dog.name);
-        dog.feed();
+
+        //System.out.println("Hi, I'm " + dog.name);
+
+        //dog.feed();
+
         Human me = new Human();
         me.firstName = "Karol";
         me.lastName = "Glaza";
         me.pet = dog;
-        me.pet.feed();
-        System.out.println(me.pet.species);
-        me.pet = new Animal("Lion");
-        me.pet.name = "Myszoskoczek";
-        me.pet.feed();
-        System.out.println(me.pet.species);
-        Animal cat = new Animal("cat");
-        cat.feed();
+        me.setSalary(2600.00);
+
+        //me.pet.feed();
+        //System.out.println(me.pet.species);
+
+        //me.pet = new Animal("Lion");
+        // me.pet.name = "Myszoskoczek";
+
+        //me.pet.feed();
+
+        //System.out.println(me.pet.species);
+
+        //Animal cat = new Animal("cat");
+        //cat.feed();
+
+        //for (int i = 0; i < 52; i++)
+        //{
+        //   cat.takeForAWalk();
+        //}
+
         Car car = new Car("Astra", "Opel");
         car.engineSize = 1.5;
         car.liczbaKoni = 138;
         car.kolor = "zielony";
 
         me.car = car;
+
+        me.setSalary(2600.0);
+        System.out.println(me.getSalary());
+        me.setSalary(-3000.0);
+        System.out.println(me.getSalary());
+
+
 
 
     }
