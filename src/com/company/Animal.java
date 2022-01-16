22dfd4cc4aadd0965117692ee2048598dcac6961
Bljek <BlackDrugsGaming@gmail.com>
2022-01-16 16:13:32 +0100

    Zadanie 1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,26 +1,51 @@
 package com.company;
 import java.io.File;
-
 public class Animal {
     final String species;
     String name;
     File pic;
     private Double weight;
+    private Boolean isDead = false;
+    static final Double DEFAULT_DOG_WEIGHT = 10.0;
+    static final Double DEFAULT_LION_WEIGHT = 30.0;
+    static final Double DEFAULT_OTHER_WEIGHT = 5.0;
 
     public Animal(String species) {
         this.species = species;
         if (species == "Dog") {
-            weight = 10.0;
+            weight = DEFAULT_DOG_WEIGHT;
         } else if (species == "Lion") {
-            weight = 180.0;
+            weight = DEFAULT_LION_WEIGHT;
         } else {
-            weight = 1.0;
+            weight = DEFAULT_OTHER_WEIGHT;
         }
 
     }
 
     void feed() {
-        weight += 0.1;
-        System.out.println("Thx bro, my weight is now " + weight);
+        if (this.isDead == true) {
+            System.out.println("Your peat is dead, you can't feed it!");
+            return;
+        } else {
+            weight += 0.1;
+            System.out.println("Thx bro, my weight is now " + weight);
+        }
+    }
+
+    void takeForAWalk() {
+        if (this.isDead == true) {
+            System.out.println("Your pet is dead, you can't walk with him.");
+            return;
+        } else {
+            weight -= 0.1;
+            System.out.println("You took your pet for a walk, it's weight now is " + weight);
+        }
+        if (weight <= 0) {
+            this.die();
+        }
+    }
+    void die() {
+        System.out.println("Your pet died!");
+        this.isDead = true;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,27 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
         Animal dog = new Animal("Dog");
         dog.name = "Hacia";
-
         System.out.println("Hi, I'm " + dog.name);
-
         dog.feed();
-
         Human me = new Human();
         me.firstName = "Karol";
         me.lastName = "Glaza";
         me.pet = dog;
-
         me.pet.feed();
         System.out.println(me.pet.species);
-
         me.pet = new Animal("Lion");
-        me.pet.name = "Myszojeleń";
-
+        me.pet.name = "Myszoskoczek";
         me.pet.feed();
 
         System.out.println(me.pet.species);
+
+        Animal cat = new Animal("cat");
+        cat.feed();
+
+        for (int i = 0; i < 52; i++)
+        {
+            cat.takeForAWalk();
+        }
+
+
     }
 }
